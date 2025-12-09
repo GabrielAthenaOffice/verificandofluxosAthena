5e5ad39f24b60c42147276ce2c3042e8273eb64b
GabrielAthenaOffice <gabrielathenaoffice@gmail.com>
2025-12-09 12:48:58 -0300

    sem cabeça para pensar

--- src/main/java/api/athena/fluxo/FluxoApplication.java
+++ src/main/java/api/athena/fluxo/FluxoApplication.java
@@ -2,8 +2,12 @@ package api.athena.fluxo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class FluxoApplication {
 
 	public static void main(String[] args) {
